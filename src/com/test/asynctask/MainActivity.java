cd06c8c47b64c7c2efab15e44956aa37f51eb84c
Vitaliy Pitvalo <vitaliy@ekreative.com>
2013-03-22 23:44:37 +0200

    refactoing

--- src/com/test/asynctask/MainActivity.java
+++ src/com/test/asynctask/MainActivity.java
@@ -10,31 +10,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends FragmentActivity implements OnClickListener {
 
-	
- 
-	ViewPager mViewPager;
+
+public class MainActivity extends FragmentActivity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		Costumcode cc = new Costumcode();
+
 		super.onCreate(savedInstanceState);
 
   	    LayoutInflater inflater = LayoutInflater.from(this);
   	    List<View> pages = new ArrayList<View>();
 	        
-
-  	    
 	    View kitchen = inflater.inflate(R.layout.kitchen, null);
 	    pages.add(kitchen);
 
 	    
 	    View radio = inflater.inflate(R.layout.radio, null);
-	    cc.loadStations(radio , this);
+		new StationLoader(radio, this);
 	    pages.add(radio);
-	    
-	    
 	    
 	    View page = inflater.inflate(R.layout.mainroom, null);
 	    pages.add(page);
@@ -43,20 +37,13 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
         
         ViewPager viewPager = new ViewPager(this);
         viewPager.setAdapter(pagerAdapter);
-        viewPager.setCurrentItem(1);     
+        viewPager.setCurrentItem(0);     
         
         setContentView(viewPager);
 	}
-	
 
 	@Override
 	public void onClick(View view) {
-				
-		HttpGetTask a =  new HttpGetTask();
-		
-		a.execute("http://192.168.1.18/house/simple_udp_client_server/tune.php?f="+view.getTag().toString());
-				
-	        // Do something
+	   new SetFrequency(view.getTag().toString());
 	}
-
 }
